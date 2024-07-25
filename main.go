cda8a1f652fc1b349e6a0a1e94a5f9739571edee
aosindo <andyovvo8@gmail.com>
2024-07-25 17:49:32 +0300

    chore: added a createboard function

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	board  := CreateBoard()
+	fmt.Println(board)
+	
+}
+
+func CreateBoard() [][]string {
+	board := make([][]string, 4)
+	for i := range board {
+		board[i] = make([]string, 4)
+		for j := range board[i] {
+			board[i][j] = " "
+		}
+	}
+
+	board[0][0] = "🙂"
+	board[0][1] = "1"
+	board[0][2] = "2"
+	board[0][3] = "3"
+	board[1][0] = "1"
+	board[2][0] = "2"
+	board[3][0] = "3"
+
+	return board
 }
