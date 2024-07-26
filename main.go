cf6127bd4004d1a02da5f540b9b53f65abfd97e6
aosindo <andyovvo8@gmail.com>
2024-07-26 21:23:50 +0300

    chore : added main contents

--- main.go
+++ main.go
@@ -1,24 +1,54 @@
 package main
 
 import (
+	"os"
 	"fmt"
 )
 
 func main() {
-	board  := CreateBoard()
+
+	var mode string
+	var attempts int
+
+	fmt.Println("Welcome To Tic-tac-toe,Please choose your preffered mode:\n1.Two Players\n2.One Player")
+	for {
+		fmt.Scan(&mode)
+		if mode == "1" {
+			TwoPlayers()
+		} else if mode == "2" {
+			OnePlayer()
+		}
+		if attempts == 4 {
+			fmt.Println("Sorry: Maximum number of attempts")
+			os.Exit(0)
+		}
+		fmt.Println("Wrong Choice.Please Choose Again")
+		attempts ++
+	}
+
+}
+
+func TwoPlayers() {
+	board := CreateBoard()
 	fmt.Println(board)
 	currentplayer := "X"
 	for {
+		fmt.Println("\033[2J\033[H")
 		PrintBoard(board)
-		row , col := GetPoints(currentplayer)
+		row, col := GetPoints(currentplayer)
 		board[row][col] = currentplayer
 		if CheckWin(board, currentplayer) {
+			fmt.Println("\033[2J\033[H")
 			PrintBoard(board)
-			fmt.Printf("Player %v Wins!!!", currentplayer)
+			fmt.Printf("Player %v Wins!!!\n", currentplayer)
 			break
 		}
-		
+		currentplayer = ChangePlayer(currentplayer)
 	}
+}
+
+func OnePlayer() {
+
 }
 
 func CreateBoard() [][]string {
@@ -41,29 +71,30 @@ func CreateBoard() [][]string {
 	return board
 }
 
-func PrintBoard(board [][]string){
+func PrintBoard(board [][]string) {
 	for _, line := range board {
 		for _, str := range line {
 			fmt.Print(str)
 			fmt.Print(" ")
 		}
 		fmt.Println()
-		
+
 	}
 }
 
-func GetPoints(currentplayer string) (int, int){
-	var col , row int
+func GetPoints(currentplayer string) (int, int) {
+	var col, row int
 	for {
 		fmt.Printf("Its Players %v turn\n", currentplayer)
 		fmt.Println("Choose the column:")
 		fmt.Scan(&col)
 		fmt.Println("Choose the Row:")
 		fmt.Scan(&row)
-		if (row == 1 || row == 2 || row == 3) &&  (col == 1 || col == 2 || col == 3) {
+		if (row == 1 || row == 2 || row == 3) && (col == 1 || col == 2 || col == 3) {
 			break
 		}
 		fmt.Println("Wrong choice of input Please choose again")
+		row, col = 0, 0
 	}
 	return row, col
 }
@@ -84,5 +115,21 @@ func CheckWin(board [][]string, player string) bool {
 	if board[3][1] == player && board[2][2] == player && board[1][3] == player {
 		return true
 	}
+	if board[1][1] == player && board[2][1] == player && board[3][1] == player {
+		return true
+	}
+	if board[1][2] == player && board[2][2] == player && board[3][2] == player {
+		return true
+	}
+	if board[1][3] == player && board[2][3] == player && board[3][3] == player {
+		return true
+	}
 	return false
-} 
+}
+
+func ChangePlayer(c string) string {
+	if c == "X" {
+		return "0"
+	}
+	return "X"
+}
